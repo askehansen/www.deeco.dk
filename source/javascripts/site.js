1769c9967c8c6978b5c3c6125e2d16248389003d
Aske Hansen <aske@deeco.dk>
2017-11-11 12:18:55 +0100

    js parallax

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
 // This is where it all goes :)
+
+function fps (val) {
+  return Math.round(1000 / val)
+}
+
+setInterval(function () {
+  window.requestAnimationFrame(function () {
+    document.querySelector('.scene-layer-back').style.transform = 'translateY(' + Math.round(window.scrollY / 4) + 'px)'
+    document.querySelector('.scene-layer-front').style.transform = 'translateY(-' + Math.round(window.scrollY / 3) + 'px)'
+  })
+}, fps(60))
