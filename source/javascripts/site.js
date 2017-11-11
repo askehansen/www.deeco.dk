782233545d80f77d43a1f5a4adcb8e5fa73ee5f8
Aske Hansen <aske@deeco.dk>
2017-11-11 13:29:46 +0100

    fix overflow

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,7 +4,7 @@ function fps (val) {
   return Math.round(1000 / val)
 }
 
-setInterval(function () {
+window.onload = setInterval(function () {
   window.requestAnimationFrame(function () {
     document.querySelector('.scene-layer-back').style.transform = 'translateY(' + Math.round(window.scrollY / 4) + 'px)'
     document.querySelector('.scene-layer-front').style.transform = 'translateY(-' + Math.round(window.scrollY / 3) + 'px)'
