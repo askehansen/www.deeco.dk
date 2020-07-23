98f0a38e630bb0c95be67b3891583643fc4fc1eb
Aske Hansen <aske@deeco.dk>
2020-07-23 13:53:33 +0800

    add heap

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -11,5 +11,7 @@ window.onload = setInterval(function () {
   })
 }, fps(120))
 
-const amplitude = require('amplitude-js')
-amplitude.getInstance().init("8f12601488b8f200803346461de509db")
+<script type="text/javascript">   
+window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};   
+heap.load("1053373017"); 
+</script>
