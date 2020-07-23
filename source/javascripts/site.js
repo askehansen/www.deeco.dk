d0697a67cad986b67eb0185b8c64ab79ce6790e2
Aske Hansen <aske@deeco.dk>
2020-07-23 13:27:15 +0800

    added amplitude

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -10,3 +10,6 @@ window.onload = setInterval(function () {
     document.querySelector('.scene-layer-front').style.transform = 'translateY(-' + Math.round(window.scrollY / 3) + 'px)'
   })
 }, fps(120))
+
+const amplitude = require('amplitude-js')
+amplitude.getInstance().init("8f12601488b8f200803346461de509db")
